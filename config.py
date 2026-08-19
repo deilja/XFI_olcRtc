d7e9106a6a267d6dcc3321ba938c502231e5f114
deilja <dereilja@gmail.com>
2026-08-19 22:42:56 +0500

    Keep configuration importable for tests and tooling

--- config.py
+++ config.py
@@ -1,16 +1,11 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-if not BOT_TOKEN:
-    raise ValueError("Не задан BOT_TOKEN в переменных окружения!")
-
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-if not ADMIN_ID:
-    raise ValueError("Не задан ADMIN_ID в переменных окружения!")
-
 DB_PATH = os.getenv("DB_PATH", "database.sqlite")
 PORT_RANGE_START = int(os.getenv("PORT_RANGE_START", "20000"))
 PORT_RANGE_END = int(os.getenv("PORT_RANGE_END", "21000"))
@@ -18,10 +13,13 @@ TUNNEL_COST = float(os.getenv("TUNNEL_COST", "150.0"))
 SERVER_IP = os.getenv("SERVER_IP", "")
 TRAFFIC_LIMIT_GB = float(os.getenv("TRAFFIC_LIMIT_GB", "10"))
 
+# 3X-UI panel
 XUI_HOST = os.getenv("XUI_HOST", "http://127.0.0.1:2053").rstrip("/")
 XUI_USERNAME = os.getenv("XUI_USERNAME", "admin")
 XUI_PASSWORD = os.getenv("XUI_PASSWORD", "")
 XUI_INBOUND_ID = int(os.getenv("XUI_INBOUND_ID", "1"))
+
+# VLESS Reality endpoint
 XUI_SERVER_PORT = int(os.getenv("XUI_SERVER_PORT", "443"))
 XUI_PUBLIC_KEY = os.getenv("XUI_PUBLIC_KEY", "")
 XUI_SHORT_ID = os.getenv("XUI_SHORT_ID", "")
